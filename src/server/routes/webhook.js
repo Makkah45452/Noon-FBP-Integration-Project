36b80659e939831db4ce9eb2fede4de8bd9485fa
Makkah45452 <makkahtraders45452@gmail.com>
2025-09-06 14:45:08 +0300

    Delete src directory

--- src/server/routes/webhook.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", (req, res) => {
-  console.log("Webhook Received:", req.body);
-  res.send("Webhook received");
-});
-
-module.exports = router;
